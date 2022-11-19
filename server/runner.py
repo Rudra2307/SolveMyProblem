0599d23c49ae96997c198bb5794e063157cd2167
Rudra2307 <67470013+Rudra2307@users.noreply.github.com>
2022-11-20 01:49:54 +0530

    server update

--- server/runner.py
+++ server/runner.py
@@ -208,13 +208,15 @@ def distance(lat1, lat2, lon1, lon2):
 
 @app.route('/api/history/<email>', methods=['GET'])
 def history(email):
-    print(email)
+    email=email.strip()
+    print(email+";")
     grievance_all = list(mongo.db.grievance.find({"user_id": email}))
-
+    # print(grievance_all)
     for i in grievance_all:
-        if 'area' not in i or i['area'] == "unpredicted":
+        if 'area' not in i or i['area'] == None:
             longitude = float(i['longitude'])
             latitude = float(i['latitude'])
+            print(latitude,longitude)
             res = getLocationDetails(latitude, longitude)
             update_location = mongo.db.grievance.find_one_and_update(
                 {'grievance_id': i["grievance_id"]}, {'$set': {"area": res}})
@@ -317,11 +319,12 @@ def getLocationDetails(latitude, longitude):
     longitude = float(longitude)
 
     gp = herepy.GeocoderReverseApi(
-        'jHffeGfyLsxEo6Jn0G77H70R8mO9BXL6xQQgrJlqe4o') 
+        '5TcSeD8CEX86f_x9Q263wOQT9DoFcV08FlwZQ7fZ5ls') 
     response = gp.retrieve_addresses([latitude, longitude])
     response = str(response)
     response = ast.literal_eval(response)
     response = response["items"][0]["address"]["label"]
+    
 
     return response
 
